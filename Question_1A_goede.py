ddbb18693be9eca31778249fc1506adbec6d2895
Luukvdhorn <luukvanderhorn@gmail.com>
2025-11-24 10:59:17 +0100

    Solution2

--- Question_1A_goede.py
+++ Question_1A_goede.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-df = pd.read_excel("DemandGroup40.xlsx")
+df_demand = pd.read_excel("DemandGroup40.xlsx")
+df_population = pd.read_excel("pop.xlsx")
 
-print(df)
+print(df_demand)
+print(df_population)
+
 
 print('Het is gelukt')
