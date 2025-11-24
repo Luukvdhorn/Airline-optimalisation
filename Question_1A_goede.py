3e733e388df47e88d710acae91c3f08300d32ad3
Luukvdhorn <luukvanderhorn@gmail.com>
2025-11-24 11:42:40 +0100

    Data_test

--- Question_1A_goede.py
+++ Question_1A_goede.py
@@ -1,10 +1,41 @@
+from openpyxl import *
+from time import *
+import numpy as np
+import gurobipy as gp
+from gurobipy import GRB
+import math
+import matplotlib.pyplot as plt
 import pandas as pd
+import os as os
 
-df_demand = pd.read_excel("DemandGroup40.xlsx")
-df_population = pd.read_excel("pop.xlsx")
+wb = load_workbook("pop.xlsx")
 
-print(df_demand)
-print(df_population)
+sheet = wb.active  
+rows = tuple(sheet.iter_rows(values_only=True))
 
+# Zoek header rij (de rij waar "City" en "Country" staan)
+for i, r in enumerate(rows):
+    if r[0] == "City":
+        header_index = i
+        break
 
-print('Het is gelukt')
+# Extract population section
+pop_rows = []
+for r in rows[header_index+1:]:
+    if r[0] is None:
+        break
+    pop_rows.append(r[0:3])   # city, 2021, 2024
+
+df_pop = pd.DataFrame(pop_rows, columns=["City", "Pop2021", "Pop2024"])
+
+# Extract GDP section
+gdp_rows = []
+for r in rows[header_index+1:]:
+    if r[4] is None:
+        break
+    gdp_rows.append(r[4:7])   # country, 2021, 2024
+
+df_gdp = pd.DataFrame(gdp_rows, columns=["Country", "GDP2021", "GDP2024"])
+
+print(df_pop)
+print(df_gdp)
